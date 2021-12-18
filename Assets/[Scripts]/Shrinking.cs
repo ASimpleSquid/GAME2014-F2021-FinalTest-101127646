d24ba439b92b7bbf842af166141480bf2bc19c48
Justin Dela Cruz <Traxiliongames@gmail.com>
2021-12-17 20:32:43 -0500

    Created a shrinking Platform

---
+++ Assets/[Scripts]/Shrinking.cs
@@ -0,0 +1,50 @@
+///////////////////////////////
+/// Shrinking.cs
+/// Justin Dela Cruz
+/// 101127646
+/// Last Modified: 2021-12-17
+/// This .cs file helps the shrink Platforms Function
+/// GAME2014 - Final
+//////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrinking : MonoBehaviour
+{
+    public float changespeed = 1;
+    Rigidbody2D rb;
+    Vector3 temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Invoke("ShrinkPlatform", 0.2f);
+            Destroy(gameObject, 2f);
+        }
+    }
+
+    void ShrinkPlatform()
+    {
+        temp = transform.localScale;
+        temp.x -= 2f * changespeed * Time.deltaTime;
+        temp.y -= 2f * changespeed * Time.deltaTime;
+        transform.localScale = temp;
+    }
+}
